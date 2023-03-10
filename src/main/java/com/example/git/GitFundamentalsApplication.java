b107150b897ee841695a8569d531bd1288225b55
Rohan <98bharadwajrohan@gmail.com>
2023-03-09 18:23:18 -0800

    Third Commit

--- src/main/java/com/example/git/GitFundamentalsApplication.java
+++ src/main/java/com/example/git/GitFundamentalsApplication.java
@@ -3,11 +3,14 @@ package com.example.git;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
+
 @SpringBootApplication
 public class GitFundamentalsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitFundamentalsApplication.class, args);
+		System.out.println("Git Changes");
 	}
 
 }
